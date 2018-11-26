203ee7e3fdd262806fb036529b78278d95f327f8
KrystianRej <krystian.rej@gmail.com>
2018-11-26 14:56:59 +0100

    update

--- src/main/java/com/crud/tasks/trello/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/trello/config/AdminConfig.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 public class AdminConfig {
-    @Value("${admin.mail}$")
+    @Value("${admin.mail}")
     private String adminMail;
 }
