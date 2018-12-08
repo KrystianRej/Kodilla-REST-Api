6fd703069da4bb933c0908af27c686d91874a30c
KrystianRej <krystian.rej@gmail.com>
2018-12-08 22:15:48 +0100

    endpoints

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -23,7 +23,7 @@ public class EmailScheduler {
     private AdminConfig adminConfig;
 
     @Scheduled(cron = "0 0 10 * * *")
-    @Scheduled(fixedDelay = 10000)
+    //@Scheduled(fixedDelay = 10000)
     public void sendInformationEmail() {
         long size = taskRepository.count();
         if(size != 1) {
